34093c9b7fd475af8d47c6a3d38da746ab4e2bda
David Moles <dmoles@berkeley.edu>
2022-04-05 15:40:54 -0700

    Configure Vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
+  server: {
+    port: 4000,
+    strictPort: true
+  },
   plugins: [vue()]
 })
