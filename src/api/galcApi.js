7bc7f479f29e73f72dcf1a077b8a71405d8eb905
David Moles <dmoles@berkeley.edu>
2022-05-27 11:59:06 -0700

    Clean up API code

--- src/api/galcApi.js
+++ src/api/galcApi.js
@@ -4,102 +4,63 @@ import camelcaseKeys from 'camelcase-keys'
 
 export default {
   facets () {
-    return allFacets()
+    return this.jsonApi.findAll('facets', { include: 'terms' })
   },
+
   items (params = {}) {
-    return allItems(params)
+    return this.jsonApi.findAll('items', { include: 'terms', ...params })
+  },
+
+  get jsonApi () {
+    delete this.jsonApi
+    const jsonApi = initJsonApi()
+    return (this.jsonApi = jsonApi)
   }
-}
-
-function allFacets () {
-  return jsonApi().findAll('facets', { include: 'terms' })
-}
-
-function allItems (params = {}) {
-  const paramsActual = { include: 'terms', ...params }
-  return jsonApi().findAll('items', paramsActual)
-}
-
-// TODO: encapsulate all this in an object?
-
-let _jsonApi = null
-
-function jsonApi () {
-  if (!_jsonApi) {
-    _jsonApi = initJsonApi()
-  }
-  return _jsonApi
 }
 
 function initJsonApi () {
-  const jsonApi = new JsonApi({ apiUrl: apiBaseUrl() })
+  const config = useConfigStore()
+  const jsonApi = new JsonApi({ apiUrl: config.baseUrl })
   jsonApi.insertMiddlewareBefore('response', camelcaseMiddleware)
-  defineFacet(jsonApi)
-  defineTerm(jsonApi)
-  defineItem(jsonApi)
+  for (const [name, attrs] of Object.entries(models)) {
+    jsonApi.define(name, attrs)
+  }
   return jsonApi
 }
 
-function defineFacet (jsonApi) {
-  jsonApi.define(
-    'facet',
-    {
-      name: '',
-      terms: { jsonApi: 'hasMany', type: 'term' }
-    }
-  )
-}
-
-function defineTerm (jsonApi) {
-  jsonApi.define(
-    'term',
-    {
-      value: '',
-      facet: { jsonApi: 'hasOne', type: 'facet' },
-      parent: { jsonApi: 'hasOne', type: 'term' },
-      children: { jsonApi: 'hasMany', type: 'term' }
-    })
-}
-
-function defineItem (jsonApi) {
-  jsonApi.define(
-    'item',
-    {
-      image: '',
-      title: '',
-      artist: '',
-      artistUrl: '',
-      date: '',
-      description: '',
-      dimensions: '',
-      series: '',
-      mmsId: '',
-      barcode: '',
-      circulation: '',
-      location: '',
-      value: '',
-      appraisalDate: '',
-      notes: '',
-      reserveDate: null,
-      createdAt: null,
-      updatedAt: null,
-      permalinkUri: null,
-      terms: { jsonApi: 'hasMany', type: 'term' }
-    }
-  )
-}
-
-function apiBaseUrl () {
-  return config().baseUrl
-}
-
-let _config = null
-
-function config () {
-  if (!_config) {
-    _config = useConfigStore()
+const models = {
+  item: {
+    image: '',
+    title: '',
+    artist: '',
+    artistUrl: '',
+    date: '',
+    description: '',
+    dimensions: '',
+    series: '',
+    mmsId: '',
+    barcode: '',
+    circulation: '',
+    location: '',
+    value: '',
+    appraisalDate: '',
+    notes: '',
+    reserveDate: null,
+    createdAt: null,
+    updatedAt: null,
+    permalinkUri: null,
+    terms: { jsonApi: 'hasMany', type: 'term' }
+  },
+  term: {
+    value: '',
+    facet: { jsonApi: 'hasOne', type: 'facet' },
+    parent: { jsonApi: 'hasOne', type: 'term' },
+    children: { jsonApi: 'hasMany', type: 'term' }
+  },
+  facet: {
+    name: '',
+    terms: { jsonApi: 'hasMany', type: 'term' }
   }
-  return _config
 }
 
 // TODO: do this on server side with JSONAPI::Serializer.set_key_transform
