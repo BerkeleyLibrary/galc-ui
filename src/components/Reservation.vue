a2fb14e232471173c34c8447fe4061f90d180ab5
David Moles <dmoles@berkeley.edu>
2022-06-09 16:04:18 -0700

    Add (broken) login implementation

--- src/components/Reservation.vue
+++ src/components/Reservation.vue
@@ -1,10 +1,14 @@
 <script setup>
+import { storeToRefs } from 'pinia'
+import { computed } from 'vue'
 import { useApiStore } from '../stores/api'
 
 // ------------------------------------------------------------
 // Store
 
-const { reserveItem } = useApiStore()
+const api = useApiStore()
+const { reserveItem } = api
+const { loginUrl, reserveItemRedirectUrl, reservingItem, pendingItem } = storeToRefs(api)
 
 // ------------------------------------------------------------
 // Properties
@@ -18,11 +22,25 @@ function tryReserve (event) {
   event.target.blur()
   reserveItem(props.item)
 }
+
+const reserving = computed(() => {
+  const reserving = reservingItem.value
+  return reserving && reserving.id === props.item.id
+})
+const pending = computed(() => {
+  const pending = pendingItem.value
+  return pending && pending.id === props.item.id
+})
 </script>
 
 <template>
-  <button v-if="available" @click="tryReserve">Reserve print</button>
+  <button v-if="reserving">Reserving…</button>
+  <button v-else-if="available" @click="tryReserve">Reserve print</button>
   <button v-else disabled>Item unavailable</button>
+  <form v-if="pending" method="post" :action="loginUrl">
+    <input type="hidden" name="origin" :value="reserveItemRedirectUrl">
+    <input type="submit" value="Login">
+  </form>
 </template>
 
 <style lang="scss">
