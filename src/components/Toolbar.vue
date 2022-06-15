12894a8dab24ed5e95db97cfb34d4fa07d57dd50
David Moles <dmoles@berkeley.edu>
2022-06-14 17:13:49 -0700

    Extract toolbar component

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { storeToRefs } from 'pinia/dist/pinia'
+import { useSessionStore } from '../stores/session'
+import { useApiStore } from '../stores/api'
+
+const { authenticated } = storeToRefs(useSessionStore())
+const { logoutUrl } = storeToRefs(useApiStore())
+
+</script>
+
+<template>
+  <nav v-if="authenticated" class="galc-toolbar-nav">
+    <ul>
+      <li>
+        <a :href="logoutUrl">Log out</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss">
+.galc-toolbar-nav {
+  ul {
+    display: flex;
+    justify-content: end;
+    gap: 0.25em;
+    padding: 0;
+    margin-top: 0;
+
+    li {
+      display: inline-block;
+    }
+  }
+}
+</style>
