e138fe62ba259c7566035d282e1aa71870d9fd7d
David Moles <dmoles@berkeley.edu>
2022-08-04 13:26:12 -0700

    Center spinner on screen

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -3,27 +3,35 @@ import spinnerUrl from '../assets/circle-notch.svg'
 </script>
 
 <template>
-  <img class="galc-spinner" alt="loading…" :src="spinnerUrl">
+  <div class="galc-spinner-container">
+    <img class="galc-spinner" alt="loading…" :src="spinnerUrl">
+  </div>
 </template>
 
 <style lang="scss">
-img.galc-spinner {
-  $img-size: 24px;
+div.galc-spinner-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
 
-  @keyframes spin {
-    100% {
-      transform: rotate(360deg);
+  img.galc-spinner {
+    $img-size: 24px;
+
+    @keyframes spin {
+      100% {
+        transform: rotate(360deg);
+      }
     }
+
+    height: $img-size !important;
+    width: $img-size !important;
+    display: block;
+
+    animation: spin 1.5s linear infinite;
+    //opacity: 1 !important;
+    //filter: invert(1);
   }
-
-  height: $img-size !important;
-  width: $img-size !important;
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-
-  animation: spin 1.5s linear infinite;
-  //opacity: 1 !important;
-  //filter: invert(1);
 }
 </style>
