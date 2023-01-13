57437d54dcd1c400310a0d8fb66d640fc8ea8e26
David Moles <dmoles@berkeley.edu>
2023-01-13 09:45:26 -0800

    Add tests for admin and preview stores

---
+++ test/stores/admin.test.ts
@@ -0,0 +1,115 @@
+import { createPinia, setActivePinia, storeToRefs } from 'pinia'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { MODE_CLOSURES, MODE_ITEMS, useAdminStore } from "../../src/stores/admin"
+
+// ------------------------------------------------------------
+// Fixture
+
+// ------------------------------
+// Mock items store
+
+const newItem = vi.fn()
+
+const itemsStore = { newItem }
+
+vi.mock('@/stores/items', () => {
+  return {
+    useItemsStore: () => itemsStore
+  }
+})
+
+// ------------------------------
+// Mock closures store
+
+const newClosure = vi.fn()
+
+const closuresStore = { newClosure }
+
+vi.mock('@/stores/closures', () => {
+  return {
+    useClosuresStore: () => closuresStore
+  }
+})
+
+// ------------------------------------------------------------
+// Tests
+
+describe('admin', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  describe('adminMode', () => {
+    it('defaults to MODE_ITEMS', () => {
+      const { adminMode } = storeToRefs(useAdminStore())
+      expect(adminMode.value).toEqual(MODE_ITEMS)
+    })
+
+    describe('showClosures', () => {
+      it('sets the mode to MODE_CLOSURES', () => {
+        const { showClosures } = useAdminStore()
+        showClosures()
+
+        const { adminMode } = storeToRefs(useAdminStore())
+        expect(adminMode.value).toEqual(MODE_CLOSURES)
+      })
+    })
+
+    describe('showItems', () => {
+      it('sets the mode to MODE_ITEMS', () => {
+        const { showClosures, showItems } = useAdminStore()
+        showClosures()
+        showItems()
+        const { adminMode } = storeToRefs(useAdminStore())
+        expect(adminMode.value).toEqual(MODE_ITEMS)
+      })
+    })
+  })
+
+  describe('showHiddenFields', () => {
+    it('defaults to true', () => {
+      const { showHiddenFields } = storeToRefs(useAdminStore())
+      expect(showHiddenFields.value).toEqual(true)
+    })
+  })
+
+  describe('createItem', () => {
+    it('creates an item', () => {
+      const { createItem } = useAdminStore()
+
+      createItem()
+      expect(newItem).toHaveBeenCalledOnce()
+    })
+
+    it('sets MODE_ITEMS if not in that mode', () => {
+      const { showClosures, createItem } = useAdminStore()
+
+      showClosures()
+      createItem()
+      const { adminMode } = storeToRefs(useAdminStore())
+      expect(adminMode.value).toEqual(MODE_ITEMS)
+    })
+  })
+
+  describe('createClosure', () => {
+    it('creates an closure', () => {
+      const { createClosure } = useAdminStore()
+
+      createClosure()
+      expect(newClosure).toHaveBeenCalledOnce()
+    })
+
+    it('sets MODE_CLOSURES if not in that mode', () => {
+      const { showItems, createClosure } = useAdminStore()
+
+      showItems()
+      createClosure()
+      const { adminMode } = storeToRefs(useAdminStore())
+      expect(adminMode.value).toEqual(MODE_CLOSURES)
+    })
+  })
+})
---
+++ test/stores/preview.test.ts
@@ -0,0 +1,47 @@
+import { createPinia, setActivePinia, storeToRefs } from 'pinia'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { usePreviewStore } from "../../src/stores/preview"
+import { Item } from "../../src/types/Item"
+
+describe('preview', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  describe('currentPreview', () => {
+    it('is initially unset', () => {
+      const { currentPreview } = storeToRefs(usePreviewStore())
+      const preview = currentPreview.value
+      expect(!!preview).toEqual(false)
+    })
+  })
+
+  describe('startPreview', () => {
+    it('starts a preview', () => {
+      const previewStore = usePreviewStore()
+
+      const item: Item = { title: 'Test', suppressed: false, terms: [] }
+      const { startPreview } = previewStore
+      startPreview(item)
+
+      const { currentPreview } = storeToRefs(usePreviewStore())
+      const preview = currentPreview.value
+      expect(preview).toEqual(item)
+    })
+  })
+
+  describe('endPreview', () => {
+    it('ends a preview', () => {
+      const previewStore = usePreviewStore()
+      const { endPreview } = previewStore
+      const { currentPreview } = storeToRefs(usePreviewStore())
+
+      const item: Item = { title: 'Test', suppressed: false, terms: [] }
+      currentPreview.value = item
+
+      endPreview()
+      const preview = currentPreview.value
+      expect(!!preview).toEqual(false)
+    })
+  })
+})
