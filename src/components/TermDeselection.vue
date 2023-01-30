99442ab228016561e1c3666416f2665fd963b3ac
David Moles <dmoles@berkeley.edu>
2023-01-30 13:50:51 -0800

    Only use Hours feature button "unselected" color on hover

--- src/components/TermDeselection.vue
+++ src/components/TermDeselection.vue
@@ -62,16 +62,6 @@ fieldset.galc-term-deselection {
     top: -9999px;
   }
 
-  img.term-deselect-icon {
-    height: 1rem;
-    width: 1rem;
-    // convert black to "Founder's Rock" #3b7ea1
-    filter: invert(43%) sepia(6%) saturate(3452%) hue-rotate(157deg) brightness(102%) contrast(95%);
-    display: inline;
-    margin-bottom: -3px;
-    margin-right: 3px;
-  }
-
   div.ds-term {
     display: contents;
 
@@ -80,23 +70,42 @@ fieldset.galc-term-deselection {
 
       $color-pacific: #46535e;
       $color-hours-services: #f2f4f7;
+      $color-hours-services-selected: #2c5e77;
 
       &:checked ~ label {
         display: block;
         font-size: 1rem;
         line-height: 1.75rem;
         font-weight: normal;
-        color: $color-pacific;
-        background-color: $color-hours-services;
+        color: white;
+        background-color: $color-hours-services-selected;
         padding: 2px 8px;
         margin: 6px 16px 6px 0;
         cursor: pointer;
 
-        transition: border-color .25s;
-        border: 1px solid $color-hours-services;
+        transition: border-color .25s, background-color .25s, color .25s;
+        border: 1px solid $color-hours-services-selected;
+
+        img.term-deselect-icon {
+          height: 1rem;
+          width: 1rem;
+          filter: invert(100%);
+          display: inline;
+          margin-bottom: -3px;
+          margin-right: 3px;
+
+          transition: filter 0.25s;
+        }
 
         &:hover {
+          color: $color-pacific;
+          background-color: $color-hours-services;
           border-color: $color-pacific;
+
+          img.term-deselect-icon {
+            // convert black to "Founder's Rock" #3b7ea1
+            filter: invert(43%) sepia(6%) saturate(3452%) hue-rotate(157deg) brightness(102%) contrast(95%);
+          }
         }
       }
     }
